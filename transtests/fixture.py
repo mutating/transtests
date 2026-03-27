268340084084a26f9417208afefdd279397b975d
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-27 16:50:44 +0300

    Add unused-ignore to type hints for Python 3.8 compatibility

--- transtests/fixture.py
+++ transtests/fixture.py
@@ -1,7 +1,8 @@
 from typing import Awaitable, Callable, Generator, TypeVar, Union
 
 try:
-    from typing import ParamSpec  # type: ignore[attr-defined]
+    # TODO: Consider to delete all unused-ignore's from this file if Python 3.8 is deleted from the CI
+    from typing import ParamSpec  # type: ignore[attr-defined, unused-ignore]
 except ImportError:  # pragma: no cover
     from typing_extensions import ParamSpec
 
@@ -14,8 +15,8 @@ FunctionParameters = ParamSpec('FunctionParameters')
 ReturnValue = TypeVar('ReturnValue')
 
 @pytest.fixture(params=['sync', 'async', 'generator'])
-def transformed(request: pytest.FixtureRequest) -> Callable[[Callable[FunctionParameters, ReturnValue]], Callable[FunctionParameters, Union[ReturnValue, Generator[ReturnValue, None, None], Awaitable[ReturnValue]]]]:  # type: ignore[valid-type]
-    def transformator_function(function: Callable[FunctionParameters, ReturnValue]) -> Callable[FunctionParameters, Union[ReturnValue, Generator[ReturnValue, None, None], Awaitable[ReturnValue]]]:  # type: ignore[valid-type]
+def transformed(request: pytest.FixtureRequest) -> Callable[[Callable[FunctionParameters, ReturnValue]], Callable[FunctionParameters, Union[ReturnValue, Generator[ReturnValue, None, None], Awaitable[ReturnValue]]]]:  # type: ignore[valid-type, unused-ignore]
+    def transformator_function(function: Callable[FunctionParameters, ReturnValue]) -> Callable[FunctionParameters, Union[ReturnValue, Generator[ReturnValue, None, None], Awaitable[ReturnValue]]]:  # type: ignore[valid-type, unused-ignore]
         if request.param == 'sync':
             return function
         if request.param == 'async':
